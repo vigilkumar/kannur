75c6d0e886d14386026d8f1e1260fe6c43b08661
vigilkumar <vigilkumartp@gmail.com>
2019-01-18 15:12:26 +0530

    my fist pro
    my fist programmmmm

---
+++ hellofunc.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<hellomake.h>
+void myPrintHelloMake(void)
+{
+printf("hello makefile!\n");
+return;
+}
---
+++ hellomake.c
@@ -0,0 +1,7 @@
+#include<hellomake.h>
+
+int main(){
+//call a function in another file
+myPrinHelloMake();
+return(0);
+}
